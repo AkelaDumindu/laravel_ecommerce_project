9acc94461aba71ffbd5c39f00098d918a57f7726
Akela Dumindu <128671284+AkelaDumindu@users.noreply.github.com>
2025-01-08 01:27:47 +0530

    updated project details

--- herbaCommerce/resources/views/livewire/product-section.blade.php
+++ herbaCommerce/resources/views/livewire/product-section.blade.php
@@ -1,19 +1,19 @@
 <div class='px-10 md:px-20 sm:px-30 py-3'>
     <!-- Brand New  -->
    @include('components.navigation.view-all',[
-       'Category' => 'Brand New'
+       'Category' => 'Vegetable Plants'
    ])
    <livewire:product-listing :category_id="0" :current_product_id="0"/>
 
-   <!-- Smartphones & laptops  -->
+   
    @include('components.navigation.view-all',[
-       'Category' => 'Digital Products'
+       'Category' => 'Home Plants'
    ])
-   <livewire:product-listing :category_id="4" :current_product_id="0"/>
+   <livewire:product-listing :category_id="5" :current_product_id="0"/>
 
    <!-- Outfits  -->
    @include('components.navigation.view-all',[
-       'Category' => 'Fashion and Apparel'
+       'Category' => 'Tea Plants'
    ])
    <livewire:product-listing :category_id="1" :current_product_id="0"/>
 
